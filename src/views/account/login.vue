8dabac5dd77aa4c34c53b72da7fc18051738965f
hufy <1063681913@qq.com>
2020-12-15 21:42:33 +0800

    路由配置，antd 配置

---
+++ src/views/account/login.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>login</h1>
+    <div class="btn">
+      <a-button>hello</a-button>
+      <a-button type="primary">
+        Primary
+      </a-button>
+      <a-button>Default</a-button>
+      <a-button type="dashed">
+        Dashed
+      </a-button>
+      <a-button type="danger">
+        Danger
+      </a-button>
+      <a-config-provider :auto-insert-space-in-button="false">
+        <a-button type="primary">
+          按钮
+        </a-button>
+      </a-config-provider>
+      <a-button type="primary">
+        按钮
+      </a-button>
+      <a-button type="link">
+        Link
+      </a-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "login"
+};
+</script>
+
+<style scoped></style>
---
+++ vue.config.js
@@ -0,0 +1,9 @@
+// vue.config.js
+module.exports = {
+  // 选项...
+  publicPath: process.env.NODE_ENV === "production" ? "" : "/",
+  // 构建项目生成的目录
+  outputDir: process.env.NODE_ENV === "production" ? "dist" : "devDist",
+  // 语法的自动检测
+  lintOnSave: true
+};
